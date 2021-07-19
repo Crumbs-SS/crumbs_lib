8eceeb7cb84ee1925da52bd417b50bbd67a5ce90
jim-brower <james.brower@smoothstack.com>
2021-07-19 14:45:43 -0700

    updated driver repository to support filtering

--- user_lib/src/main/java/com/crumbs/lib/repository/DriverRepository.java
+++ user_lib/src/main/java/com/crumbs/lib/repository/DriverRepository.java
@@ -2,9 +2,21 @@ package com.crumbs.lib.repository;
 
 
 import com.crumbs.lib.entity.Driver;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DriverRepository extends JpaRepository<Driver, Long> {
+
+    @Query("select d from Driver d where d.state.state = ?1 and d.userDetails.username like ?2%")
+    Page<Driver> findAllByStatusAndSearchString(String status, String searchString, Pageable pageable);
+
+    @Query("select d from Driver d where d.userDetails.username like ?1%")
+    Page<Driver> findAllBySearchString(String searchString, Pageable pageable);
+
+    @Query("select d from Driver d where d.state.state = ?1")
+    Page<Driver> findAllByStatus(String status, Pageable pageable);
 }
