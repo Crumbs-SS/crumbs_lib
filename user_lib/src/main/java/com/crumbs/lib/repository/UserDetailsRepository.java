7ab47e7524ed4deb23d33f64670c31005850f0b9
elijahcbrooks <elijahcbrooks@gmail.com>
2021-07-20 06:14:19 -0500

    filtering for roles

--- user_lib/src/main/java/com/crumbs/lib/repository/UserDetailsRepository.java
+++ user_lib/src/main/java/com/crumbs/lib/repository/UserDetailsRepository.java
@@ -1,6 +1,8 @@
 package com.crumbs.lib.repository;
 
 import com.crumbs.lib.entity.UserDetails;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,5 +14,8 @@ public interface UserDetailsRepository extends JpaRepository<UserDetails, Long> 
     Optional<UserDetails> findByUsername(String username);
     Optional<UserDetails> findByEmail(String email);
 
-    Page<UserDetails> findByRole(Pageable pageable, String role);
+    Page<UserDetails> findUserDetailsByAdminIsNotNull(Pageable pageable);
+    Page<UserDetails> findUserDetailsByCustomerIsNotNull(Pageable pageable);
+    Page<UserDetails> findUserDetailsByDriverIsNotNull(Pageable pageable);
+    Page<UserDetails> findUserDetailsByOwnerIsNotNull(Pageable pageable);
 }
