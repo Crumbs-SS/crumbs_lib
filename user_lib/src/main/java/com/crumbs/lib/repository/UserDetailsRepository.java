1a4886903634658e3176cce7c32405e4056d40f7
elijahcbrooks <elijahcbrooks@gmail.com>
2021-07-21 02:19:26 -0500

    queries for filtering

--- user_lib/src/main/java/com/crumbs/lib/repository/UserDetailsRepository.java
+++ user_lib/src/main/java/com/crumbs/lib/repository/UserDetailsRepository.java
@@ -5,17 +5,32 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserDetailsRepository extends JpaRepository<UserDetails, Long> {
-    Optional<UserDetails> findByUsernameOrEmail(String username, String email);
-    Optional<UserDetails> findByUsername(String username);
-    Optional<UserDetails> findByEmail(String email);
+public interface UserDetailsRepository extends JpaRepository<UserDetails, Long>, QueryByExampleExecutor<UserDetails> {
+   String query = " AND (u.username LIKE CONCAT('%',:query,'%')" +
+           " OR u.firstName LIKE CONCAT('%',:query,'%') OR u.lastName LIKE CONCAT('%',:query,'%')" +
+           "OR u.email LIKE CONCAT('%',:query,'%'))";
 
-    Page<UserDetails> findUserDetailsByAdminIsNotNull(Pageable pageable);
-    Page<UserDetails> findUserDetailsByCustomerIsNotNull(Pageable pageable);
-    Page<UserDetails> findUserDetailsByDriverIsNotNull(Pageable pageable);
-    Page<UserDetails> findUserDetailsByOwnerIsNotNull(Pageable pageable);
+
+   String admin = "SELECT u FROM UserDetails u JOIN u.admin us WHERE us IS NOT NULL" + query;
+   String customer = "SELECT u FROM UserDetails u JOIN u.customer us WHERE us IS NOT NULL" + query;
+   String owner = "SELECT u FROM UserDetails u JOIN u.owner us WHERE us IS NOT NULL" + query;
+   String driver = "SELECT u FROM UserDetails u JOIN u.driver us WHERE us IS NOT NULL" + query;
+
+   Optional<UserDetails> findByUsernameOrEmail(String username, String email);
+   Optional<UserDetails> findByUsername(String username);
+   Optional<UserDetails> findByEmail(String email);
+
+   @Query(admin)
+   Page<UserDetails> findByAdmin(Pageable pageable, String query);
+   @Query(customer)
+   Page<UserDetails> findByCustomer(Pageable pageable, String query);
+   @Query(driver)
+   Page<UserDetails> findByDriver(Pageable pageable, String query);
+   @Query(owner)
+   Page<UserDetails> findByOwner(Pageable pageable, String query);
 }
